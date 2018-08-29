b3eb2c53100132f95355324c909f2915b72da07f
siopow sisig <ievsantillan@gmail.com>
2018-08-29 18:06:33 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Text classifcation & Sentiment Analysis on IMDB's dataset.',
+    author='ian santillan',
+    license='MIT',
+)
